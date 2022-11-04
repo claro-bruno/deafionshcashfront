f178395d02e81444d89fe1c2711d169209d61bb4
bruno <brunofay1@hotmail.com>
2022-11-04 18:12:29 -0300

    FIX vite env variable to deploy

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getUserFromLocalStorage } from '../context/AuthProvider/utils'
 
 export const Api = axios.create({
-  baseURL: import.meta.env.API_ROUTE,
+  baseURL: import.meta.env.VITE_API_ROUTE,
 })
 
 Api.interceptors.request.use(
